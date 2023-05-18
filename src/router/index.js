71888387a530ac799ce9afd48e48f04623fa96d1
NikolayP <nikipanovski@gmail.com>
2023-05-18 15:37:28 +0200

    Add Register user page (visually only; will not implement against DB)

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterViewMock from "../views/RegisterViewMock.vue"
 import LoginView from "../views/LoginView.vue"
 import CreateNewProjectView from "../views/operations/CreateNewProjectView.vue"
 import CreateNewTaskView from "../views/operations/CreateNewTaskView.vue"
@@ -20,6 +21,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/test/register',
+      name: 'register',
+      component: RegisterViewMock
     },
     {
       path: '/login',
---
+++ src/views/RegisterViewMock.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="flex flex-col">
+        <div class="relative w-1/2 mx-auto my-1">
+            <label for="inputFirstName"
+                class="block">First name:</label>
+            <input type="text" id="inputFirstName" placeholder="First name"
+                class="block w-full rounded-sm m-1 p-1 text-black">
+        </div>
+
+        <div class="relative w-1/2 mx-auto my-1">
+            <label for="inputLastName"
+                class="block">Last name:</label>
+            <input type="text" id="inputLastName" placeholder="Last name"
+                class="block w-full rounded-sm m-1 p-1 text-black">
+        </div>
+
+        <div class="relative w-1/2 mx-auto my-1">
+            <label for="inputEmail"
+                class="block">Email:</label>
+            <input type="email" id="inputEmail" placeholder="Your email here"
+                class="block w-full rounded-sm m-1 p-1 text-black">
+        </div>
+        <div class="relative w-1/2 mx-auto my-1">
+            <label for="inputPassword"
+                class="block">Password:</label>
+            <input type="password" id="inputPassword" placeholder="Your password here"
+                class="block w-full rounded-sm m-1 p-1 text-black">
+        </div>
+
+        <!-- COMPANY: Regular text (for string compare) in this app,
+            would need to be its own DB struct (here: advanced input/dropdown) in a production application. -->
+        <div class="relative w-1/2 mx-auto my-1">
+            <label for="inputCompany"
+                class="block">Company:</label>
+            <input type="text" id="inputCompany" placeholder="Company"
+                class="block w-full rounded-sm m-1 p-1 text-black">
+        </div>
+
+        <br/>
+
+        <button type="button" class="w-1/3 bg-sky-400 rounded-lg
+                                    relative mx-auto my-8 py-4 left-0 right-0
+                                    text-lg font-semibold text-white">Register</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
