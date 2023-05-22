420172b2a9c162366b97bf99a5afc8389c3cc64e
NikolayP <nikipanovski@gmail.com>
2023-05-22 10:40:30 +0200

    Send current user info to CurrentUserHeader component

--- src/components/app_header/CurrentUserHeader.vue
+++ src/components/app_header/CurrentUserHeader.vue
@@ -4,8 +4,11 @@
     <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" 
       class="my-2" />
 
-    <div v-if=currentUser class="flex basis-full flex-row justify-around">
-      <LoggedInInfoActions />
+    <div v-if=currentUser.hasCurrentUser class="flex basis-full flex-row justify-around">
+      <LoggedInInfoActions v-bind:name_first="currentUser.name_first"
+                           v-bind:name_last="currentUser.name_last"
+                           v-bind:company="currentUser.company"
+                           v-bind:role="currentUser.role" />
     </div>
     <div v-else
       class="flex flex-row-reverse place-items-start">
@@ -15,16 +18,10 @@
 </template>
 
 <script setup>
-  import { ref } from "vue";
   import LoggedOutInfoActions from './LoggedOutInfoActions.vue';
   import LoggedInInfoActions from './LoggedInInfoActions.vue';
+  import { useCurrentUserStore } from "../currentUserStore.js";
 
-  // send current user info here
+  const currentUser = useCurrentUserStore();
 
-  
-  const currentUser = ref();
-
-  function updateCurrentUser(newUserInfo) {
-
-  }
 </script>
