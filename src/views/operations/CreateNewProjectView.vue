5d874fdcd8b2ad297862d88f48c1e1e9340729c5
NikolayP <nikipanovski@gmail.com>
2023-05-22 19:03:07 +0200

    Update "Create new project" view with functionality

--- src/views/operations/CreateNewProjectView.vue
+++ src/views/operations/CreateNewProjectView.vue
@@ -5,25 +5,25 @@
 
         <label for="inputNewProject"
             class="">Name</label>
-        <input type="text" id="inputNewProject" placeholder="New Project"
+        <input type="text" id="inputNewProject" placeholder="New Project" v-model="name" required
             class="rounded-sm m-1 p-1 text-black">
 
         <label for="inputNewDescription"
             class="">Description</label>
-        <input type="text" id="inputNewDescription" placeholder="Sample description"
+        <input type="text" id="inputNewDescription" placeholder="Sample description" v-model="description"
             class="rounded-sm m-1 p-1 text-black">
 
         <label for="inputStakeholderCompany"
             class="">Company</label>
-        <input type="text" id="inputStakeholderCompany" placeholder="Stakeholder Company"
+        <input type="text" id="inputStakeholderCompany" placeholder="Stakeholder Company" v-model="company" required
             class="rounded-sm m-1 p-1 text-black">
 
         <label for="inputTimeEstimated"
             class="">Time Estimated</label>
         <div >
-            <input id="inputTimeEstimated" type="number" min="0" max="99999" step="1" 
+            <input id="inputTimeEstimated" type="number" min="0" max="99999" step="1" v-model="hours" required
                 class="rounded-sm m-1 p-1 text-black" />h : 
-            <input type="number" min="0" max="59" step="1" 
+            <input type="number" min="0" max="59" step="1" v-model="minutes" required
                 class="rounded-sm m-1 p-1 text-black" />m
         </div>
 
@@ -31,18 +31,42 @@
             class="">Deadline</label>
             <!-- TODO?: v-bind: value to default to now? -->
             <!-- TODO: v-bind: min and max to visually create acceptable range for Deadline (from current time to project deadline) -->
-        <input type="date" id="inputTimeDeadline" value="" min="" max=""
+        <input type="date" id="inputTimeDeadline" min="" max="" v-model="deadline" required
             class="rounded-sm m-1 p-1 text-black">
 
 
-        <button type="button" class="w-1/3 bg-sky-400 rounded-lg
+        <button type="button" @click="tryCreateProject()"
+                            class="w-1/3 bg-sky-400 rounded-lg
                                 relative mx-auto my-8 py-4 left-0 right-0
                                 text-lg font-semibold text-white">Create New Project</button>
     </div>
 </template>
 
 <script setup>
+    import { ref } from "vue";
+    import createNewProjectFunctions from "@/components/createNewProject.js";
 
+    const name = ref("");
+    const description = ref("");
+    const company = ref("");
+    const hours = ref(0);
+    const minutes = ref(0);
+    const deadline = ref();
+    const { createNewProject } = createNewProjectFunctions();
+
+    function tryCreateProject() {
+        const frontendProjectParams = { 
+            name: name.value,
+            description: description.value,
+            company: company.value,
+            time_estimated: hours.value * 60 + minutes.value,
+            deadline: deadline.value
+        }
+
+        createNewProject(frontendProjectParams);
+
+        // router.push( { path: view of "all projects" page } );
+    }
 </script>
 
 <style scoped>
